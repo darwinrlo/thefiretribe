2c8278ead8f5e07bc80d28ce0b437fe119dd83e0
Darwin Lo <darwinrlo@gmail.com>
2022-02-26 19:21:23 -0800

    Take a shorthand and redirect to the target link

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import *
+
+full_links = {
+    'sowmya': 'https://coda.io/d/Airlift-Sowmya-Darwin_dQn7W1Has1v/Todos_sutqD#_lu9QK'
+}
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<shorthand>")
+def redirect_shorthand(shorthand):
+    if shorthand not in full_links:
+        abort(404)
+    return redirect(full_links[shorthand])
